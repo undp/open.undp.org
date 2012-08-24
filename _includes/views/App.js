0e16ea95bba90042845a8d1a262bb5b5d3cb262d
mattg <greenem2@msu.edu>
2012-08-24 10:22:32 -0400

    map + all points

--- _includes/views/App.js
+++ _includes/views/App.js
@@ -15,6 +15,7 @@ views.App = Backbone.View.extend({
     },
     render: function() {
         this.$el.empty().append(templates.app(this));
+        this.buildMap();
         return this;
     },
     setFilter: function(e) {
@@ -46,6 +47,36 @@ views.App = Backbone.View.extend({
 
         e.preventDefault();
         app.navigate(path, { trigger: true });
+    },
+    buildMap: function() {
+        var locations = [];
+        $.getJSON('api/operating-unit-index.json', function(data) {
+            for (var i = 0; i < data.length; i++) {
+                var o = data[i];
+                if (o.lon) {
+                    locations.push({
+                        geometry: {
+                            coordinates: [
+                                o.lon,
+                                o.lat]
+                        },
+                        properties: {
+                            id: o.id,
+                            name: o.name,
+                            'marker-color': '#316593'
+                        }
+                    });
+                }
+            }
+        });
+    
+        mapbox.auto('map', 'mapbox.mapbox-light', function(map) {
+            var markersLayer = mapbox.markers.layer();
+            mapbox.markers.interaction(markersLayer);
+            markersLayer.features(locations);
+            map.addLayer(markersLayer);
+            map.extent(markersLayer.extent());
+        });
     },
     toggleChart: function (e) {
         var $target = $(e.target);
