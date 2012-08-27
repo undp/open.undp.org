673e687966026a72b62d56456ef67deaace1e170
Dave Cole <dave@developmentseed.org>
2012-08-27 15:29:17 -0400

    Reorder map building functions

--- _includes/views/Map.js
+++ _includes/views/Map.js
@@ -16,33 +16,12 @@ views.Map = Backbone.View.extend({
             unit = (this.collection) ? this.collection 
                 : this.model.get('operating_unit'),
             objCheck = _.isObject(unit);
-        
-        $.getJSON('api/operating-unit-index.json', function(data) {
-            for (var i = 0; i < data.length; i++) {
-                var o = data[i];
-                if ((objCheck) ? unit.operating_unit[o.id] && o.lon : o.id === unit && o.lon) {
-                    locations.push({
-                        geometry: {
-                            coordinates: [
-                                o.lon,
-                                o.lat]
-                        },
-                        properties: {
-                            id: o.id,
-                            title: o.name,
-                            'marker-color': '#316593',
-                            count: (objCheck) ? unit.operating_unit[o.id] : false,
-                            budget: (objCheck) ? unit.operating_unitBudget[o.id] : that.model.get('budget')
-                        }
-                    });
-                }
-            }
-        });
-        
+
         mapbox.auto(this.el, 'mapbox.mapbox-light', function(map) {
             map.ui.zoomer.remove();
             map.ui.attribution.remove();
-            
+            map.setZoomRange(2, 17);
+
             var radii = function(f) {
                 return clustr.area_to_radius(
                     Math.round(f.properties.budget / 100000)
@@ -51,11 +30,36 @@ views.Map = Backbone.View.extend({
 
             var markers = mapbox.markers.layer()
                 .factory(clustr.scale_factory(radii, "rgba(2,56,109,0.6)", "#01386C"));
-            mapbox.markers.interaction(markers);
-            markers.features(locations);
-            map.extent(markers.extent());
-            if (locations.length === 1){map.zoom(4);}
-            map.addLayer(markers);
+
+            $.getJSON('api/operating-unit-index.json', function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    var o = data[i];
+                    if ((objCheck) ? unit.operating_unit[o.id] && o.lon : o.id === unit && o.lon) {
+                        locations.push({
+                            geometry: {
+                                coordinates: [
+                                    o.lon,
+                                    o.lat
+                                ]
+                            },
+                            properties: {
+                                id: o.id,
+                                title: o.name,
+                                'marker-color': '#316593',
+                                count: (objCheck) ? unit.operating_unit[o.id] : false,
+                                budget: (objCheck) ? unit.operating_unitBudget[o.id] : that.model.get('budget')
+                            }
+                        });
+                    }
+                }
+                markers.features(locations);
+                mapbox.markers.interaction(markers);
+                map.extent(markers.extent());
+                if (locations.length === 1){map.zoom(4);}
+                map.addLayer(markers);
+    
+            });
+
         });
     }
 });
