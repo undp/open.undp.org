4856625d47d4f4a49d77d9af05dc8591edc1b90d
Nate Smith <nate@developmentseed.org>
2012-08-29 14:04:41 -0400

    reduced donor types to unique types in project summary

--- bin/index.py
+++ bin/index.py
@@ -16,11 +16,16 @@ project_list = []
 row_count = 0
 for pval in iter(project_sort):
     row_count = row_count + 1
+    donorType = []
+    for type in pval['donor_types'].split(','):
+        if type not in donorType:
+            donorType.append(type)
+    print donorType
     project = {
         "budget": float(pval['budget']),
         "crs": pval['crs'],
         "donors": pval['donors'].split(','),
-        "donor_types": pval['donor_types'].split(','),
+        "donor_types": donorType,
         "expenditure": float(pval['expenditure']),
         "focus_area": pval['focus_area'],
         "id": pval['id'],
