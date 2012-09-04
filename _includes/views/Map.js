98d7c5f7cadc04e00c80bfee9d732dcdb0b5d394
Dave Cole <dave@developmentseed.org>
2012-09-04 16:54:50 -0400

    Remove click handler for map; it's too prone to misfire during panning

--- _includes/views/Map.js
+++ _includes/views/Map.js
@@ -1,7 +1,4 @@
 views.Map = Backbone.View.extend({
-    events: {
-        'click img.mapmarker': 'mapClick'
-    },
     initialize: function() {
         this.render();
         if (this.collection) {
@@ -12,10 +9,6 @@ views.Map = Backbone.View.extend({
         this.$el.empty().append('<div class="inner-shadow"></div>');
         this.buildMap();
         return this;
-    },
-    mapClick: function(e) {
-        var $target = $(e.target);
-        window.location = '#filter/operating_unit-' + $target.attr('id');
     },
     buildMap: function() {
         var that = this,
