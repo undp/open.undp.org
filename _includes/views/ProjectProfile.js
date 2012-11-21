15d3df368e5bed5c1c211ed8bec2c85785dbf8d3
Dave Cole <dave@developmentseed.org>
2012-11-21 14:39:28 -0500

    About the data modal on project profiles

--- _includes/views/ProjectProfile.js
+++ _includes/views/ProjectProfile.js
@@ -26,7 +26,7 @@ views.ProjectProfile = Backbone.View.extend({
 
     render: function() {
         $('#breadcrumbs ul').html(
-            '<li><a href="#">Home</a></li>' +
+            '<li><a href="http://www.undp.org/content/undp/en/home.html">Home</a></li>' +
             '<li><a href="' + BASE_URL + '">Our Projects</a></li>' +
             '<li><a href="#filter/operating_unit-' + this.model.get('operating_unit_id') + '">' + this.model.get("operating_unit") + '</a></li>' +
             '<li><a href="#project/' + this.model.get('id') + '">' + this.model.get('id') + '</a></li>'
