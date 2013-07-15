13fd753d0c96c85a3bd3f524d5d2c7b006bf9f5b
akshaysinha <akshay.sinha@undp.org>
2013-07-15 23:40:45 +0700

    commit from test's repo

--- _includes/views/ProjectProfile.js
+++ _includes/views/ProjectProfile.js
@@ -1,188 +1,222 @@
 views.ProjectProfile = Backbone.View.extend({
     events: {
-        'click .widget-config': 'requestIframe',
-        'click .load a': 'loadMore'
-    },
+		'click .widget-config': 'requestIframe',
+		'click .load a': 'loadMore'
+	},
 
-    initialize: function() {
-        this.render();
-        var outputID = this.options.gotoOutput;
-        if (outputID) {
-            window.setTimeout(function() { window.scrollTo(0, $('#output-' + outputID).offset().top); }, 0);
+	initialize: function() {
+		this.render();
+		var outputID = this.options.gotoOutput;
+		if (outputID) {
+			window.setTimeout(function() { window.scrollTo(0, $('#output-' + outputID).offset().top); }, 0);
+		}
+
+		$('#all-projects').on('click', function(e) {
+			if (app.app) {
+				e.preventDefault();
+				window.history.back();
+			}
+		});
+
+		$('#profile .summary').removeClass('off');
+		this.low = 10,
+		this.high = 10;
+	},
+
+	render: function() {
+		$('#breadcrumbs ul').html(
+			'<li><a href="http://open.undp.org/">Home</a></li>' +
+			'<li><a href="' + BASE_URL + '">Our Projects</a></li>' +
+			'<li><a href="#filter/operating_unit-' + this.model.get('operating_unit_id') + '">' + this.model.get("operating_unit") + '</a></li>' +
+			'<li><a href="#project/' + this.model.get('id') + '">' + this.model.get('id') + '</a></li>'
+		);
+		/*var start =[];
+		var end =[];
+		if(this.model.get('start').indexOf('/')>=0)
+		{
+			start = this.model.get('start').split('/');
+		}
+		if(this.model.get('start').indexOf('-')>=0)
+		{
+			start = this.model.get('start').split('-');
+		}
+		if(this.model.get('end').indexOf('/')>=0)
+		{
+			end = this.model.get('end').split('/');            
+		}
+		if(this.model.get('end').indexOf('-')>=0)
+		{
+			end = this.model.get('end').split('-');
+		}*/
+		var start = this.model.get('start').split('/');
+		var end = this.model.get('end').split('/');
+
+		var startDate = new Date(start[1],start[0]-1,start[2]),
+			endDate = new Date(end[1],end[0]-1,end[2]),
+			curDate = new Date(),
+			progress = ((curDate - startDate) / (endDate - startDate)) * 100;
+			that = this;
+
+		this.model.attributes.budget = _.chain(this.model.attributes.outputs)
+			.map(function (o) { return o.budget; })
+			.flatten()
+			.reduce(function(memo, num){ return memo + num; }, 0)
+			.value();
+
+		this.model.attributes.expenditure = _.chain(this.model.attributes.outputs)
+			.map(function (o) { return o.expenditure; })
+			.flatten()
+			.reduce(function(memo, num){ return memo + num; }, 0)
+			.value();
+
+		this.model.attributes.budgetyears = _.reduce(this.model.attributes.outputs, function (res, obj) {
+			_.each(obj.fiscal_year, function(o,i) {
+				res[o] = (res[o] || 0) + obj.budget[i];
+			});
+			return res;
+			},{});
+
+		this.model.attributes.expendyears = _.reduce(this.model.attributes.outputs, function (res, obj) {
+			_.each(obj.fiscal_year, function(o,i) {
+				res[o] = (res[o] || 0) + obj.expenditure[i];
+			});
+			return res;
+			},{});
+		/*var s=[];
+		var e=[];
+		if(this.model.get('start').indexOf('/')>=0)
+		{
+			s = this.model.get('start').split('/');
+		}
+		if(this.model.get('start').indexOf('-')>=0)
+		{
+			s = this.model.get('start').split('-');
+		}
+		if(this.model.get('end').indexOf('/')>=0)
+        {
+            e = this.model.get('end').split('/');            
         }
+        if(this.model.get('end').indexOf('-')>=0)
+        {
+            e = this.model.get('end').split('-');
+        }*/
+		var s = this.model.get('start').split('/');
+		 var e = this.model.get('end').split('/');
 
-        $('#all-projects').on('click', function(e) {
-            if (app.app) {
-                e.preventDefault();
-                window.history.back();
-            }
-        });
+		var start = new Date(s[1],s[0]-1,s[2]).format('M d, Y');
+		var end = new Date(e[1],e[0]-1,e[2]).format('M d, Y');
 
-        $('#profile .summary').removeClass('off');
-        this.low = 10,
-        this.high = 10;
-    },
+		// Filter out any image files from showing up
+		var documents = [];
 
-    render: function() {
-        $('#breadcrumbs ul').html(
-            '<li><a href="http://www.undp.org/content/undp/en/home.html">Home</a></li>' +
-            '<li><a href="' + BASE_URL + '">Our Projects</a></li>' +
-            '<li><a href="#filter/operating_unit-' + this.model.get('operating_unit_id') + '">' + this.model.get("operating_unit") + '</a></li>' +
-            '<li><a href="#project/' + this.model.get('id') + '">' + this.model.get('id') + '</a></li>'
-        );
+		if (this.model.get('document_name')) {
+			var filterDocuments = _(this.model.get('document_name')[1]).filter(function(d) {
+				return !(/\.(gif|jpg|jpeg|tiff|png)$/i).test(d);
+			});
 
-        var start = this.model.get('start').split('-');
-        var end = this.model.get('end').split('-');
+			if (filterDocuments.length !== 0) {
+				_(filterDocuments).each(function(d, i) {
+					documents[i] = {};
+					var title = (d.split('/').pop()).split(/(.)[^.]*$/)[0].replace('_', ' ');
+					if (title.length > 38) {
+						documents[i].title = title.substring(0, 38) + '...';
+					} else {
+						documents[i].title = title;
+					}
+					documents[i].filetype = d.split('.').pop();
+					documents[i].src = d;
+				});
+			}
+		}
 
-        var startDate = new Date(start[0],start[1]-1,start[2]),
-            endDate = new Date(end[0],end[1]-1,end[2]),
-            curDate = new Date(),
-            progress = ((curDate - startDate) / (endDate - startDate)) * 100;
-            that = this;
+		window.setTimeout(function() { $('html, body').scrollTop(0); }, 0);
 
-        this.model.attributes.budget = _.chain(this.model.attributes.outputs)
-            .map(function (o) { return o.budget; })
-            .flatten()
-            .reduce(function(memo, num){ return memo + num; }, 0)
-            .value();
+		if (this.options.embed) {
+			this.$el.empty().append(templates.embedProjectProfile({
+				start: this.model.get('start'),
+                		end: this.model.get('end'),
+				documents: documents,
+				model: this.model
+			}));
 
-        this.model.attributes.expenditure = _.chain(this.model.attributes.outputs)
-            .map(function (o) { return o.expenditure; })
-            .flatten()
-            .reduce(function(memo, num){ return memo + num; }, 0)
-            .value();
+			// Depending on the options passed into the array add a fade
+			// in class to all elements containing a data-option attribute
+			if (this.options.embed) {
+				_(this.options.embed).each(function (o) {
+					$('[data-option="' + o + '"]').show();
+				});
+			}
 
-        this.model.attributes.budgetyears = _.reduce(this.model.attributes.outputs, function (res, obj) {
-            _.each(obj.fiscal_year, function(o,i) {
-                res[o] = (res[o] || 0) + obj.budget[i];
-            });
-            return res;
-            },{});
+		} else {
+			this.$el.empty().append(templates.projectProfile({
+				start: this.model.get('start'),
+                		end: this.model.get('end'),
+				base: BASE_URL,
+				documents: documents,
+				model: this.model
+			})).show();
+		}
 
-        this.model.attributes.expendyears = _.reduce(this.model.attributes.outputs, function (res, obj) {
-            _.each(obj.fiscal_year, function(o,i) {
-                res[o] = (res[o] || 0) + obj.expenditure[i];
-            });
-            return res;
-            },{});
+		// If first load is a project page or output, don't animate
+		if (app.app && this.options.gotoOutput === false) {
+			$('#profile .summary').addClass('off');
+		}
 
-        var s = this.model.get('start').split('-');
-        var e = this.model.get('end').split('-');
+		this.map = new views.ProjectMap({
+			el: '#profilemap',
+			model: this.model,
+			render: true
+		});
 
-        var start = new Date(s[0],s[1]-1,s[2]).format('M d, Y');
-        var end = new Date(e[0],e[1]-1,e[2]).format('M d, Y');
+		$('#progress').find('.bar').css('width', progress + '%');
 
-        // Filter out any image files from showing up
-        var documents = [];
+		this.$('#outputs').empty();
+		if (this.model.attributes.outputs) {
+			var outputs = this.model.attributes.outputs.slice(0, 9);
+			_(outputs).each(function(model) {
+				this.$('#outputs').append(templates.projectOutputs({ model: model }));
+			});
+	
+			// Project Outputs
+			if (this.model.attributes.outputs.length < 10) {
+				$('.load').hide();
+			} else {
+				$('.load').show();
+			}
+		}
 
-        if (this.model.get('document_name')) {
-            var filterDocuments = _(this.model.get('document_name')[1]).filter(function(d) {
-                return !(/\.(gif|jpg|jpeg|tiff|png)$/i).test(d);
-            });
+		// Append menu items to the breadcrumb
+		$('breadcrumbs').find('ul').remove();
 
-            if (filterDocuments.length !== 0) {
-                _(filterDocuments).each(function(d, i) {
-                    documents[i] = {};
-                    var title = (d.split('/').pop()).split(/(.)[^.]*$/)[0].replace('_', ' ');
-                    if (title.length > 38) {
-                        documents[i].title = title.substring(0, 38) + '...';
-                    } else {
-                        documents[i].title = title;
-                    }
-                    documents[i].filetype = d.split('.').pop();
-                    documents[i].src = d;
-                });
-            }
-        }
+		return this;
+	},
 
-        window.setTimeout(function() { $('html, body').scrollTop(0); }, 0);
+	requestIframe: function() {
+		var context = $('#widget');
 
-        if (this.options.embed) {
-            this.$el.empty().append(templates.embedProjectProfile({
-                start: start,
-                end: end,
-                documents: documents,
-                model: this.model
-            }));
+		// Reset things each time the widget
+		// is requested to the page.
+		widgetOpts = []
+		$('.widget-preview', context).html('<h3 class="empty">To use this widget choose some options on the left.</h3>');
+		$('.widget-code', context).hide();
+		$('.widget-options a', context).removeClass('active');
+	},
 
-            // Depending on the options passed into the array add a fade
-            // in class to all elements containing a data-option attribute
-            if (this.options.embed) {
-                _(this.options.embed).each(function (o) {
-                    $('[data-option="' + o + '"]').show();
-                });
-            }
+	loadMore: function(e) {
+		this.low = this.high;
+		this.high += 10;
 
-        } else {
-            this.$el.empty().append(templates.projectProfile({
-                start: start,
-                end: end,
-                base: BASE_URL,
-                documents: documents,
-                model: this.model
-            })).show();
-        }
+		var outputs = this.model.attributes.outputs.slice(this.low, this.high);
 
-        // If first load is a project page or output, don't animate
-        if (app.app && this.options.gotoOutput === false) {
-            $('#profile .summary').addClass('off');
-        }
+		if (outputs.length) {
+			_(outputs).each(function(model) {
+				this.$('#outputs').append(templates.projectOutputs({ model: model }));
+			});
+		} else {
+			$(e.target).text('All Projects Loaded').addClass('disabled');
+		}
 
-        this.map = new views.ProjectMap({
-            el: '#profilemap',
-            model: this.model,
-            render: true
-        });
-
-        $('#progress').find('.bar').css('width', progress + '%');
-
-        this.$('#outputs').empty();
-        if (this.model.attributes.outputs) {
-            var outputs = this.model.attributes.outputs.slice(0, 9);
-            _(outputs).each(function(model) {
-                this.$('#outputs').append(templates.projectOutputs({ model: model }));
-            });
-    
-            // Project Outputs
-            if (this.model.attributes.outputs.length < 10) {
-                $('.load').hide();
-            } else {
-                $('.load').show();
-            }
-        }
-
-        // Append menu items to the breadcrumb
-        $('breadcrumbs').find('ul').remove();
-
-        return this;
-    },
-
-    requestIframe: function() {
-        var context = $('#widget');
-
-        // Reset things each time the widget
-        // is requested to the page.
-        widgetOpts = []
-        $('.widget-preview', context).html('<h3 class="empty">To use this widget choose some options on the left.</h3>');
-        $('.widget-code', context).hide();
-        $('.widget-options a', context).removeClass('active');
-    },
-
-    loadMore: function(e) {
-        this.low = this.high;
-        this.high += 10;
-
-        var outputs = this.model.attributes.outputs.slice(this.low, this.high);
-
-        if (outputs.length) {
-            _(outputs).each(function(model) {
-                this.$('#outputs').append(templates.projectOutputs({ model: model }));
-            });
-        } else {
-            $(e.target).text('All Projects Loaded').addClass('disabled');
-        }
-
-        return false;
-    }
+		return false;
+	}
 
 });
