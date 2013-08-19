27f8fc05108c376eecf2031a6daa76941213ecd5
davejohn <johnson@mapbox.com>
2013-08-19 18:02:52 -0400

    adding the new python script which only export project JSONs at the moment

---
+++ bin/undpnew.py
@@ -0,0 +1,418 @@
+
+import time, csv, json,  os, re, sys, wget, requests
+from lxml import etree
+from itertools import groupby
+from datetime import datetime
+from sys import argv
+from sets import Set
+
+# set the file location
+projects_file = 'download/atlas_projects_small.xml'
+
+def nodecpy(out, node, name, attrs={}, convert=unicode):
+    if ((node is None) or (node.text is None)):
+        return
+    if node.text:
+        out[name] = convert(node.text)
+    for k, v in attrs.items():
+    	try:
+            out[name + '_' + v] = node.get(k)
+        except AttributeError:
+            pass
+
+def getValue(value):
+    try:
+        return float(value)
+    except ValueError:
+        nicevalue = re.sub(",","",value)
+        return float(nicevalue)
+
+def getFieldsData(fields, activity, out):
+    for field in fields:
+        xpath = field[0]
+        fieldname = field[1]
+        attribs = dict([(k, k) for k in field[2]])
+        nodecpy(out, activity.find(xpath), fieldname, attribs)
+
+def parse_tx(tx):
+    out = {}
+    value = tx.find('value')
+    if value is not None:
+        out['value_date'] = value.get('value-date')
+        out['value_currency'] = value.get('currency')
+        out['value'] = getValue(value.text)
+    fields = [
+      ('description', 'description', {}),
+      ('transaction-type', 'transaction_type', {'code'}),
+      ('flow-type', 'flow_type', {'code'}),
+      ('finance-type', 'finance_type', {'code'}),
+      ('tied-status', 'tied_status', {'code'}),
+      ('aid-type', 'aid_type', {'code'}),
+      ('disbursement-channel', 'disbursement_channel', {'code'}),
+      ('provider-org', 'provider_org', {'ref'}),
+      ('receiver-org', 'receiver_org', {'ref'})
+            ]
+    
+    getFieldsData(fields, tx, out)
+
+    for date in tx.findall('transaction-date'):
+        try:
+            # for some (WB) projects, the date is not set even though the tag exists...
+            if (date is not None):
+                temp = {}
+                nodecpy(temp, date,
+                    'date',
+                    {'iso-date': 'iso-date'})
+
+                date_iso_date = date.get('iso-date')
+
+                if (date_iso_date):
+                    d = (date_iso_date)
+                    out['transaction_date_iso'] = d
+                elif (temp.has_key('date')):
+                    d = (temp['date'])
+                    out['transaction_date_iso'] = d
+
+            else:
+                print "No date!!"
+           
+        except ValueError:
+            pass
+
+    if not (out.has_key('transaction_date_iso')):
+        out['transaction_date_iso'] = out['value_date']
+    return out
+
+def outputsLoop(event, n):
+	row_count = 0
+	outputs = []
+	outputsFull = []
+	outputsHeader = ['output_id','award_id','output_title','output_descr','gender_id','gender_descr','focus_area','focus_area_descr','crs','crs_descr','fiscal_year','budget','expenditure','donor_id','donor_short','donor_name','donor_type_id','donor_type','donor_country_id','donor_country','donor_budget','donor_expend']
+	for out,output in groupby(outputs_sort, lambda x: x['projectID']): 
+	    row_count = row_count + 1
+	    outputList = [out]
+	    outputAward = []
+	    outputTitle = []
+	    outputDescr = []
+	    outputGenID = []
+	    outputGenDescr = []
+	    outputFA = []
+	    outputFAdescr = []
+	    outputCRS = []
+	    outputCRSdescr = []
+	    outputFY = []
+	    outputBudget = []
+	    outputExpend = []
+	    for o in output:
+	        if o['awardid'] not in outputAward:
+	            outputAward.append((o['awardid'] if o['awardid'] != "" else "-"))
+	        if o['project_description'] not in outputTitle:
+	            outputTitle.append((o['project_description'] if o['project_description'] != "" else "-"))
+	        if o['project_med_de'] not in outputDescr:
+	            outputDescr.append((o['project_med_de'] if o['project_med_de'] != "" else "-"))
+	        if o['gender_marker'] not in outputGenID:
+	            outputGenID.append((o['gender_marker'] if o['gender_marker'] != "" else "-"))
+	        if o['gender_marker_descr'] not in outputGenDescr:
+	            outputGenDescr.append((o['gender_marker_descr'] if o['gender_marker_descr'] != "" else "-"))
+	        if o['focus_area'] not in outputFA:
+	            outputFA.append((o['focus_area'] if o['focus_area'] != "" else "-"))
+	        if o['fa_description'] not in outputFAdescr:
+	            outputFAdescr.append((o['fa_description'] if o['fa_description'] != "" else "-"))
+	        if o['crs'] not in outputCRS:
+	            outputCRS.append((o['crs'] if o['crs'] != "" else "-"))
+	        if o['crs_descr'] not in outputCRSdescr:
+	            outputCRSdescr.append((o['crs_descr'] if o['crs_descr'] != "" else "-"))
+	        if o['fiscal_year'] not in outputFY:
+	            outputFY.append((o['fiscal_year'] if o['fiscal_year'] != "" else "-"))
+	        outputBudget.append((float(o['budget']) if o['budget'] != "" else 0))
+	        outputExpend.append((float(o['expenditure']) if o['expenditure'] != "" else 0))
+	    outputList.append(outputAward)
+	    outputList.append(outputTitle)
+	    outputList.append(outputDescr)
+	    outputList.append(outputGenID)
+	    outputList.append(outputGenDescr)
+	    outputList.append(outputFA)
+	    outputList.append(outputFAdescr)
+	    outputList.append(outputCRS)
+	    outputList.append(outputCRSdescr)
+	    outputList.append(outputFY)
+	    outputList.append(outputBudget)
+	    outputList.append(outputExpend)
+	    for dOut in donorOutputs:
+	        if dOut['outputID'] == out:
+	            outputList.append(dOut['donorID'])
+	            outputList.append(dOut['donorShort'])
+	            outputList.append(dOut['donorName'])
+	            outputList.append(dOut['donorTypeID'])
+	            outputList.append(dOut['donorType'])
+	            outputList.append(dOut['donorCtyID'])
+	            outputList.append(dOut['donorCty'])
+	            outputList.append(dOut['donorBudget'])
+	            outputList.append(dOut['donorExpend'])
+	    outputsFull.append(dict(zip(outputsHeader,outputList))) # this returns a list of dicts of output informaiton for each output
+
+def getaddresstags(tags):
+    addr_tags = []
+    for t in tags:
+        key = t.get('k')
+        if key.split(':') == 'addr':
+            addr_tags.append(t.attrib)
+    return addr_tags
+    print addr_tags
+
+
+projects = []
+projectsFull = []
+projectsSmallFull = []
+# projectsHeader = ['project_id','project_title','project_descr','inst_id','inst_descr','inst_type_id','inst_type_descr','fiscal_year','start','end','operating_unit_id','operating_unit','region_id','region_name','outputs','document_name','subnational']
+projectsHeader = ['project_id','project_title','project_descr','start','end','inst_id','inst_descr','inst_type_id','inst_type_descr','subnational','operating_unit','operating_unit_id']
+projectsSmallHeader = ['project_id','title','subnational']
+
+# def getSubnational():
+# 	subnational = csv.DictReader(open('download/undp_export/subnational.csv','rb'), delimiter = ',', quotechar = '"')
+# 	subnational_sort = sorted(subnational, key = lambda x: x['awardID'])
+# 	return subnational_sort
+
+# def getUnits():
+# 	units = csv.DictReader(open('download/undp_export/report_units.csv', 'rb'), delimiter = ',', quotechar = '"')
+# 	units_sort = sorted(units, key = lambda x: x['operating_unit'])
+# 	return units_sort
+
+# def getBureau():
+# 	bureau = csv.DictReader(open('download/undp_export/regions.csv', 'rb'), delimiter = ',', quotechar = '"')
+# 	bureau_sort = sorted(bureau, key = lambda x: x['bureau'])
+# 	return bureau_sort
+
+def loopData(file_name, key):
+	# Get CSVs 
+	subnational = csv.DictReader(open('download/undp_export/subnational.csv','rb'), delimiter = ',', quotechar = '"')
+	subnational_sort = sorted(subnational, key = lambda x: x['awardID'])
+	units = csv.DictReader(open('download/undp_export/report_units.csv', 'rb'), delimiter = ',', quotechar = '"')
+	units_sort = sorted(units, key = lambda x: x['operating_unit'])
+	bureau = csv.DictReader(open('download/undp_export/regions.csv', 'rb'), delimiter = ',', quotechar = '"')
+	bureau_sort = sorted(bureau, key = lambda x: x['bureau'])
+
+
+
+	# Get IATI activities XML
+	context = iter(etree.iterparse(file_name,tag='iati-activity'))
+	
+	# Loop through each IATI activity in the XML
+	row_count = 0
+	projectSmallList = {}
+	projects = []
+	# for tx in activity.findall("transaction"):
+	# 	transaction = parse_tx(tx)
+	# 	transaction['iati_identifier'] = out['iati_identifier']
+	# 	missingfields(transaction, Transaction, package_filename)
+	# 	t = Transaction(**transaction)
+	# 	db.session.add(t)
+	# missingfields(out, Activity, package_filename)
+	# x = Activity(**out) 
+	# db.session.add(x)
+	# return (out)
+	for event, p in context:
+		for tx in p.findall("transaction"):
+			transaction = parse_tx(tx)
+		
+		# hierarchy used to determine if output or input1
+		hierarchy = p.attrib['hierarchy']
+		current = p.attrib['default-currency']
+		awardArray = p[1].text.split('-', 2)
+		award = awardArray[2]
+		implement = p.find("./participating-org[@role='Implementing']")
+		if hierarchy == '2':
+			# Send the outputs to a separate function, to be joined to their projects later.
+			print  "   "
+			print "output"
+			print  "   " 
+
+			# if loc['awardID'] == award or str(loc['awardID']) == award[3:]:
+				# 	locationTemp = {}
+				# 	locationTemp['lat'] = float(loc['lat'])
+				# 	locationTemp['lon'] = float(loc['lon'])
+				# 	locationTemp['type'] = loc['type']
+				# 	locationTemp['precision'] = loc['precision']
+				# 	locationTemp['scope'] = loc['scope']
+				# 	subnationalTemp.append(locationTemp)
+			
+			projectList.append(subnationalTemp)
+
+			subnat = p.findall('location')
+			for loc in subnat:
+				locationTemp = {}
+				coordinates = loc.find('./coordinates').attrib
+				locationTemp['lat'] = float(coordinates.get('latitude'))
+				locationTemp['lon'] = float(coordinates.get('longitude'))
+				locationTemp['type'] = loc.find('./location-type').text
+				locationTemp['precision'] = coordinates.get('precision')
+				locationTemp['name'] = loc.find('./name').text
+				locationTemp['descrip'] = loc.find('./description').text
+				subnationalTemp.append(locationTemp)
+				
+		if hierarchy == '1':
+			print  "   "
+			print "project"
+			print  "   "
+
+# ['project_id',
+# 'project_title',
+# 'project_descr',
+# 'inst_id',
+# 'inst_descr',
+# 'inst_type_id',
+# 'inst_type_descr',
+# 'fiscal_year',
+# 'start','end',
+# 'operating_unit_id',
+# 'operating_unit',
+# 'region_id',
+# 'region_name',
+# 'outputs',
+# 'document_name',
+# 'subnatio 	nal']
+
+			row_count = row_count + 1
+			projectList = [award]
+			projectSmallList['id'] = award
+			docTemp = []
+			subnationalTemp = []
+			award_title = p.find("./title").text
+			award_description = p.find("./description").text
+
+			# institutionid =  p.find("./")	value = tx.find('value')
+			# 	if value is not None:
+			# 		out['value_date'] = value.get('value-date')
+		
+			#institutionid = inst_descr.get('ref')
+			#x = [ b.tag for b in p.iterfind("./participating-org[@role='Implementing']") ]
+			
+			# inst = (p.iterfind("./participating-org[@role='Implementing']"))
+			
+			inst_type_descr = []
+			projectFY = [] 
+			start_date = p.find("./activity-date[@type='start-planned']").text
+			end_date = p.find("./activity-date[@type='end-planned']").text
+			op_unit = p.find("./recipient-country").attrib
+			operatingunit = op_unit.get('code')
+			ou_descr = p.find("./recipient-country").text
+			bureau = []
+			institutionid = []
+			bureau_description = []
+			# for p in project:
+			#     if p['award_title'] not in award_title:
+			#         award_title.append(p['award_title'])
+			#     if p['award_description'] not in award_description:
+			#         award_description.append(p['award_description'])
+			#     if p['institutionid'] not in institutionid:
+			#         institutionid.append(p['institutionid'])
+			#     if p['inst_descr'] not in inst_descr:
+			#         inst_descr.append(p['inst_descr'])
+			#     if p['inst_type'] not in inst_type:
+			#         inst_type.append(p['inst_type'])
+			#     if p['inst_type_descr'] not in inst_type_descr:
+			#         inst_type_descr.append(p['inst_type_descr'])
+			#     if p['fiscal_year'] not in projectFY:
+			#         projectFY.append(p['fiscal_year'])
+			#     if p['start_date'] not in start_date:
+			#         start_date.append(p['start_date'].rstrip(' 00:00:00.0'))
+			#     if p['end_date'] not in end_date:
+			#         end_date.append(p['end_date'].rstrip(' 00:00:00.0'))
+			#     for op in units_sort:
+			#         if op['operating_unit'] == p['operatingunit']:
+			#             if p['operatingunit'] not in operatingunit:
+			#                 operatingunit.append(p['operatingunit'])
+			#             if op['ou_descr'] not in ou_descr:
+			#                 ou_descr.append(op['ou_descr'])
+			#     for b in bureau_sort:
+			#         if b['bureau'] == p['bureau']:
+			#             if p['bureau'] not in bureau:
+			#                 bureau.append(p['bureau'])
+			#             if b['bureau_description'] not in bureau_description:
+			#                 bureau_description.append(b['bureau_description'])
+			projectList.append(award_title)
+			projectList.append(award_description)
+			projectList.append(start_date)
+			projectList.append(end_date)
+			try: 
+				inst = p.find("./participating-org[@role='Implementing']").attrib
+				inst_descr = p.find("./participating-org[@role='Implementing']").text
+				institutionid = inst.get('ref')
+				inst_type_id = inst.get('type')
+				projectList.append(institutionid)
+				projectList.append(inst_descr)
+				projectList.append(inst_type_id)
+			except: 
+				pass
+			# projectList.append(inst_type_descr)
+			projectList.append(subnationalTemp)
+			projectList.append(projectFY)
+			projectList.append(ou_descr)
+			projectList.append(operatingunit)
+			
+			projectList.append(bureau)
+			projectList.append(bureau_description)
+			
+			# outputTemp = []
+			# for out in outputsFull:
+			#     if out['award_id'] == award:
+			#         outputTemp.append(out)
+			# projectList.append(outputTemp)
+			# for doc in docProjects:
+			#     if doc['projectID'] == award:
+			#         docTemp.append(doc['docName'])
+			#         docTemp.append(doc['docURL'])
+			# projectList.append(docTemp)
+			
+				
+			projectsFull.append(dict(zip(projectsHeader,projectList))) # this joins project information, output per project, and documents for each project
+			
+			# # Add info to smaller object for op unit JSONs
+			# projectSmallList['title'] = award_title
+			# projectSmallList['op_unit'] = operatingunit
+			# projectSmallList['subnational'] = subnationalTemp
+			# projectsSmallFull.append(projectSmallList)
+
+
+loopData(projects_file,'document-link')
+
+# Generate JSONs for each project
+file_count = 0
+for row in projectsFull:
+	file_count = file_count + 1
+	writeout = json.dumps(row, sort_keys=True, separators=(',',':'))
+	f_out = open('../api/projects_new/%s.json' % row['project_id'], 'wb')
+	f_out.writelines(writeout)
+	f_out.close()
+print '%d project files generated...' % file_count
+
+
+
+# # print root.tag
+# # def loadActivity(activity):
+# #     activity['description'] = list(activity['description'])
+# #     activity['participating-org'] = list(activity['participating-org'])
+# #     activity['addr_chg_nd'] = list(changeset['addr_chg_nd'])
+# #     activity['addr_chg_way'] = list(changeset['addr_chg_way'])
+# #     url = 'http://api.openstreetmap.org/api/0.6/changeset/%s' % changeset['id']
+# #     r = requests.get(url)
+# #     if not r.text: return changeset
+# #     t = etree.fromstring(r.text.encode('utf-8'))
+# #     activity['details'] = dict(t.find('.//changeset').attrib)
+# #     comment = t.find(".//tag[@k='comment']")
+# #     created_by = t.find(".//tag[@k='created_by']")
+# #     if comment is not None: changeset['comment'] = comment.get('v')
+# #     if created_by is not None: changeset['created_by'] = created_by.get('v')
+    
+# #     return activity
+
+# # loadActivity()
+
+# # t = etree.fromstring(r.text.encode('utf-8'))
+# # print t
+# # # tree = etree.parse(filename)
+# # # root = tree.getroot()
+
+# # # for child in root:
+# # # 	for child2 in child:
+# # # 		print child2.tag, child2.attrib
