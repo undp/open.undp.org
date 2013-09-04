8bf5f9fd31fb00d3a851f679635458bb2585aca9
davejohn <johnson@mapbox.com>
2013-09-04 13:30:51 -0400

    adding more small countries for conditional country zoom

--- _includes/views/Map.js
+++ _includes/views/Map.js
@@ -162,10 +162,10 @@ views.Map = Backbone.View.extend({
                     } else {
                         // Determine zoom level based on size of country
                         var bigCntry = "CHN USA ARG﻿ BRA"
-                        var smlCntry = "PHL SVK NPL BTN BLZ BLR LTU﻿ LIE LSO BRB"
+                        var smlCntry = "PHL SVK NPL BTN BLZ BLR LTU﻿ LIE LSO BRB GUY﻿ AFG TJK SUR﻿ GUF ECU BOL PRY URY PAN CRI GTM HND NIC HTI DOM CUB SLV"
                         if (parent.id == 'RUS'){cntryZoom = 2}
                         else if (bigCntry.indexOf(parent.id) > -1){cntryZoom = 3}
-                        else if (smlCntry.indexOf(parent.id) > -1){cntryZoom = 5;
+                        else if (smlCntry.indexOf(parent.id) > -1){cntryZoom = 5}
                         else {cntryZoom = 4}
                         view.map.setView([parent.lat,parent.lon],cntryZoom); //why is the lat and lon reversed here
 
