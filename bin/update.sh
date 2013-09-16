92d584048e2feb2f0c7056a9e61f93b62c75ca53
MateoV <matt@developmentseed.org>
2013-09-16 17:47:35 -0400

    clean up files & shell scripts

--- bin/update.sh
+++ bin/update.sh
@@ -5,9 +5,4 @@
 
 echo "Starting update process..."
 # Run the python script to process and produce the API JSON files
-python undp-process.py
-
-# Run a zip command to update the zipped download file with new data
-cd download/undp_export
-zip ../../../download/undp-project-data.zip report_projects.csv report_units.csv donor_gross.csv donor_local.csv report_documents.csv report_donors.csv report_outputs.csv 
-
+python undp-process-xml.py
