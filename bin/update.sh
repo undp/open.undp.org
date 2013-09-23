f1054dd6f9a20b31712e0c9d4b639dd8e2f2a717
Nate Smith <nate@developmentseed.org>
2013-09-23 17:41:38 -0400

    adjusting update script and zip file

--- bin/update.sh
+++ bin/update.sh
@@ -9,5 +9,5 @@ python undp-process-xml.py
 
 # Run a zip command to update the zipped download file with new data
 cd download/undp_export
-zip ../../../download/undp-project-data.zip atlas_projects.xml country_donors_updated.csv report_donors.csv report_units.csv donor_gross.csv donor_local.csv ../../hdi/hdi-csv-clean.csv
+zip -j ../../../download/undp-project-data.zip atlas_projects.xml country_donors_updated.csv report_donors.csv report_units.csv donor_gross.csv donor_local.csv ../../hdi/hdi-csv-clean.csv
 
