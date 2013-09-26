3d9364586904944cf084169efe206a0662a95086
akshaysinha <asinha82@gmail.com>
2013-09-26 10:32:15 +0800

    Update Map.js

--- _includes/views/Map.js
+++ _includes/views/Map.js
@@ -44,7 +44,7 @@ views.Map = Backbone.View.extend({
             scrollWheelZoom: wheelZoom
             });
             
-        view.map.legendControl.addLegend('last update');
+        //view.map.legendControl.addLegend('last update');
 
         //for IE 8 and above add country outline
         if (!IE || IE_VERSION > 8){view.outline = new L.GeoJSON()};
