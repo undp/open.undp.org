8ed8b3fdd8e0f728ade3abf39783bd3e00b627ab
jueyang <jueyang22@gmail.com>
2013-12-06 15:08:13 -0500

    correct breadcrumb link on project page

--- _includes/views/ProjectProfile.js
+++ _includes/views/ProjectProfile.js
@@ -28,8 +28,8 @@ views.ProjectProfile = Backbone.View.extend({
         $('#breadcrumbs ul').html(
             '<li><a href="http://www.undp.org/content/undp/en/home.html">Home</a></li>' +
             '<li><a href="' + BASE_URL + '">Our Projects</a></li>' +
-            '<li><a href="/filter/operating_unit-' + this.model.get('operating_unit_id') + '">' + this.model.get("operating_unit") + '</a></li>' +
-            '<li><a href="#project/' + this.model.get('id') + '">' + this.model.get('id') + '</a></li>'
+            '<li><a href="' + BASE_URL + '#'+ CURRENT_YR +'/filter/operating_unit-' + this.model.get('operating_unit_id') + '">' + this.model.get("operating_unit") + '</a></li>' +
+            '<li><a href="' + BASE_URL + '#project/' + this.model.get('id') + '">' + this.model.get('id') + '</a></li>'
         );
 
         var start = this.model.get('start').split('-');
