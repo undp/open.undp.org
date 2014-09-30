2bfd2e94101940257d5b2423b6bb76343f2252ac
jueyang <jueyang22@gmail.com>
2014-09-30 11:36:57 -0400

    widget view template loading + cleanup

--- _includes/views/Widget.js
+++ _includes/views/Widget.js
@@ -1,6 +1,6 @@
 views.Widget = Backbone.View.extend({
     el: '#widget',
-
+    template: _.template($('#widgetTemplate').html()),
     events: {
        'click .widget-options a': 'widgetOptions',
        'click .widget-code': 'inputSelect'
@@ -12,16 +12,14 @@ views.Widget = Backbone.View.extend({
     },
 
     render: function(keypress) {
-        var view = this;
+        this.$el.html(this.template());
 
-        $(this.el).empty().append(templates.widget());
-
-        if (view.options.context === 'projects') {
-            $('.proj-opt', view.$el).hide();
-            $('.main-opt', view.$el).show();
+        if (this.options.context === 'projects') {
+            $('.proj-opt', this.$el).hide();
+            $('.main-opt', this.$el).show();
         } else {
-            $('.main-opt', view.$el).hide();
-            $('.proj-opt', view.$el).show();
+            $('.main-opt', this.$el).hide();
+            $('.proj-opt', this.$el).show();
         }
         // add custom donor content option when a donor country is filtered
         if (global.app) {
@@ -39,50 +37,49 @@ views.Widget = Backbone.View.extend({
 
     destroy: function() {
         this.undelegateEvents();
-        $(this.el).removeData().unbind();
+        this.$el.removeData().unbind();
     },
 
     widgetOptions: function(e) {
-        var view = this;
-            view.path = '#widget/';
+        this.path = '#widget/';
 
         if (location.hash.split('/').length === 1) {
-            view.path = location.hash + '/widget/';
+            this.path = location.hash + '/widget/';
         } else {
-            view.path = location.hash
+            this.path = location.hash
                 .replace('filter', 'widget')
                 .replace('project', 'widget/project');
         }
 
         var widgetAnchors = $('.widget-options').find('a.active'),
             widgetOpts = [],
-            $el = $(e.target),
-            opt = $el.attr('data-value');
+            $optionEl = $(e.target),
+            opt = $optionEl.attr('data-value');
 
         _(widgetAnchors).each(function(anchor){
             var widgetTitle = $(anchor).attr('data-value');
             widgetOpts.push(widgetTitle);
         })
 
-        if ($el.hasClass('active')) {
-            $el.removeClass('active');
+        if ($optionEl.hasClass('active')) {
+            $optionEl.removeClass('active');
             widgetOpts.splice(widgetOpts.indexOf(opt), 1);
         } else {
-            $el.addClass('active');
+            $optionEl.addClass('active');
             widgetOpts.push(opt);
         }
 
         if (widgetOpts.length !== 0) {
-            view.widgetCode = '<iframe src="{{site.baseurl}}/' +
-                'embed.html' + view.path + '?' + widgetOpts.join('&') +
+            widgetCode = '<iframe src="{{site.baseurl}}/' +
+                'embed.html' + this.path + '?' + widgetOpts.join('&') +
                 '" width="100%" height="100%" frameborder="0"> </iframe>';
 
-            $('.widget-preview', view.$el).html(view.widgetCode);
+            $('.widget-preview', this.$el).html(widgetCode);
             $('.widget-code', view.$el)
-                .val(view.widgetCode.replace('src="{{site.baseurl}}/','src="' + Backbone.history.location.origin + '/'))
+                .val(widgetCode.replace('src="{{site.baseurl}}/','src="' + Backbone.history.location.origin + '/'))
                 .select();
         } else {
-            $('.widget-preview', view.$el).html('<h3 class="empty">To use this widget choose options from above.</h3>');
+            $('.widget-preview', this.$el).html('<h3 class="empty">To use this widget choose options from above.</h3>');
         }
 
         return false;
