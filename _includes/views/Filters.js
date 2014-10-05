d99f686038228bc225bd875fb191eb0804fbaefc
jueyang <jueyang22@gmail.com>
2014-10-05 14:04:27 -0400

    improve readability of Filters.js

--- _includes/views/Filters.js
+++ _includes/views/Filters.js
@@ -3,16 +3,19 @@ views.Filters = Backbone.View.extend({
     initialize: function () {
         this.subTemplate = _.template($('#filterItem').html());
         this.collection.on('update', this.render, this);
+        _.bindAll(this,'render','renderFilters');
     },
     render: function(keypress) {
         var view = this;
+
         setTimeout(function() {
 
-            var filterModels = [],
-                chartModels = [],
-                donor = '';
+            view.filterModels = [];
+            view.chartModels = [];
+                donor;
 
-            var activeFilter = view.collection.findWhere({active:true});
+            var isThereDonorCountry = _(global.processedFacets).findWhere({ collection: 'donor_countries' }),
+                activeFilter = view.collection.findWhere({active:true});
 
             $('#' + view.collection.id).toggleClass('filtered', false);
 
@@ -28,40 +31,55 @@ views.Filters = Backbone.View.extend({
                 $('#' + activeFilter.collection.id).toggleClass('filtered', true);
     
                 // copy the collection twice for different usage
-                filterModels.push(activeFilter);
-                chartModels.push(activeFilter);
-                filterCallback();
+                view.filterModels.push(activeFilter);
+                view.chartModels.push(activeFilter);
+
+                view.renderFilters(keypress);
 
             } else {
                 view.collection.sort();
                 
+                // when looping through the budget source collection
+                // define donorCountry
+                // if it exists, set it as the id of the country
+                // if it does not exist, set it to false
                 if (view.collection.id === 'donors') {
-                    var donorCountry = _(global.processedFacets).where({ collection: 'donor_countries' });
-                    donorCountry = (donorCountry.length) ? donorCountry[0].id : false;
-                    view.donorCountry = donorCountry;
+                    if (_.isObject(isThereDonorCountry)){
+                        view.donorCountry = isThereDonorCountry.id;
+                    } else {
+                        view.donorCountry = false;
+                    }
                 }
 
                 setTimeout(function() {
-                    filterModels = view.collection.chain().filter(function(model) {
+                    view.filterModels = view.collection.filter(function(model) {
                         // Filter donors on active donor country
-                        var donorCountryFilter = (donorCountry) ? (model.get('country') === donorCountry) : true;
+                        var matchingDonorCountry;
 
-                        return (model.get('visible') && model.get('count') > 0 && donorCountryFilter);
+                        if (view.donorCountry) {
+                            matchingDonorCountry = (model.get('country') === view.donorCountry);
+                        } else {
+                            matchingDonorCountry = true;
+                        }
 
-                    }).value();
-                    filterCallback();
+                        return (model.get('visible') && model.get('count') > 0 && matchingDonorCountry);
+
+                    });
+
+                    view.renderFilters(keypress);
+
                 }, 0);
 
-                if (donorCountry) {
-                    chartModels = view.collection.chain()
+                if (view.donorCountry) {
+                    view.chartModels = view.collection
                         .filter(function(model) {
-                            return (model.get('country') === donorCountry);
-                        }).value();
+                            return (model.get('country') === view.donorCountry);
+                        });
                 } else if (view.collection.id == 'donors'){
                     // Creating chartModels array for top budget sources, filter through 
                     // more countries since the budget sources are calculated below, 
                     // resulting in a different number than budget
-                    chartModels = view.collection.chain()
+                    view.chartModels = view.collection.chain()
                         .sortBy(function(model) {
                             return -1 * model.get('expenditure') || 0;
                         })
@@ -72,7 +90,7 @@ views.Filters = Backbone.View.extend({
                         .value(); // Top 20
                 } else {
                     // Top 20 donors, donor_countries, and operating_unit
-                    chartModels = view.collection.chain()
+                    view.chartModels = view.collection.chain()
                         .sortBy(function(model) {
                             return -1 * model.get('expenditure') || 0;
                         })
@@ -83,50 +101,10 @@ views.Filters = Backbone.View.extend({
                         .value(); // Top 20
                 }
             }
-
-            function filterCallback() {
-                if (filterModels.length) {
-                    view.$el.html(view.template(view)); // pass in the view as the template varible
-
-                    _(filterModels).each(function(model) {
-
-                        view.$('.filter-items').append(view.subTemplate({model:model}));
-                        $('#' + view.collection.id + '-' + model.id).toggleClass('active', model.get('active'));
-
-                        if (model.get('active') && !keypress) {
-                            new views.Breadcrumbs({
-                                add: 'activeFilter',
-                                filterName: model.get('name').toLowerCase().toTitleCase(),
-                                filterCollection: view.collection.id,
-                                filterId: model.get('id')
-                            });
-
-                            new views.Description({
-                                facetName:view.collection.id,
-                                activeModel:model,
-                                donorCountry: model.get('name')
-                            })
-                        }
-                    });
-                } else {
-                    view.$el.empty();
-                }
-
-                var facets = new Facets().idsOnly();
-                // when all the facets have been looped
-                // update the map the description
-                if (global.filtercounter !== facets.length ) {
-                    global.filtercounter = (global.filtercounter) ? global.filtercounter + 1 : 2;
-                } else {
-                    global.filtercounter = 0;
-                    if (!keypress) global.projects.map.render();
-                    if (!keypress) global.updateDescription();
-                }
-
-            }
     
             // Root path of links for each chart item
             var pathTo;
+
             if (global.processedFacets.length === 0 ){
                 pathTo = '#' + CURRENT_YR +'/filter/';
             } else {
@@ -138,7 +116,7 @@ views.Filters = Backbone.View.extend({
             pathTo = BASE_URL + pathTo;
 
             //If we don't have any data for that chart don't render it
-            if (chartModels.length <= 1 && view.collection.id !== 'focus_area' && !donorCountry) {
+            if (view.chartModels.length <= 1 && view.collection.id !== 'focus_area' && !view.donorCountry) {
                 $('#chart-' + view.collection.id)
                     .css('display','none');
             }  
@@ -161,18 +139,61 @@ views.Filters = Backbone.View.extend({
                     return filter.collection === 'donor_countries';
                 }) || {id: 0}).id;
 
+            // chart functions live in Chart.js
             if (view.collection.id === 'focus_area') {
-                chartModels = view.collection.models;
-                setTimeout(function() {renderFocusAreaChart(chartModels, pathTo, view)},0);
+                view.chartModels = view.collection.models;
+                setTimeout(function() {
+                    renderFocusAreaChart(view.chartModels, pathTo, view)
+                },0);
             } else if ( view.collection.id === 'donors' ){
-                view.chartModels = chartModels;
-                setTimeout(function() {renderBudgetSourcesChart(donor, donor_ctry, chartModels, view, pathTo)},0);
+                setTimeout(function() {
+                    renderBudgetSourcesChart(donor, donor_ctry, view.chartModels, view, pathTo)
+                },0);
             } else if (view.collection.id === 'operating_unit' || view.collection.id === 'donor_countries') {
-                view.chartModels = chartModels;
-                setTimeout(function() {renderRecipientOfficesChart(donor, donor_ctry, chartModels, view, pathTo) },0);
+                setTimeout(function() {
+                    renderRecipientOfficesChart(donor, donor_ctry, view.chartModels, view, pathTo)
+                },0);
             }
-        
         }, 0);
+    },
+    renderFilters: function(keypress){
+        if (this.filterModels.length) {
+            this.$el.html(this.template(this)); // pass in the view as the template varible
+
+            _(this.filterModels).each(function(model) {
+
+                this.$('.filter-items').append(this.subTemplate({model:model}));
+                $('#' + this.collection.id + '-' + model.id).toggleClass('active', model.get('active'));
+
+                if (model.get('active') && !keypress) {
+                    new views.Breadcrumbs({
+                        add: 'activeFilter',
+                        filterName: model.get('name').toLowerCase().toTitleCase(),
+                        filterCollection: this.collection.id,
+                        filterId: model.get('id')
+                    });
+
+                    new views.Description({
+                        facetName:this.collection.id,
+                        activeModel:model,
+                        donorCountry: model.get('name')
+                    })
+                }
+            },this);
+        } else {
+            this.$el.empty();
+        }
+
+        var facetLeng = new Facets().idsOnly();
+        // when all the facets have been looped
+        // update the map the description
+        if (global.filtercounter !== facetLeng.length ) {
+            global.filtercounter = (global.filtercounter) ? global.filtercounter + 1 : 2;
+        } else {
+            global.filtercounter = 0;
+            if (!keypress) global.projects.map.render();
+            if (!keypress) global.updateDescription();
+        }
     }
 });
 
