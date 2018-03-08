4bb6ce08301d4c6b802f67cbd80726afe0eabb08
Jura Khrapunov <jura@khrapunov.com>
2018-03-08 18:15:12 -0500

    Updated PO source and labeling

--- _includes/views/ProjectProfile.js
+++ _includes/views/ProjectProfile.js
@@ -229,7 +229,7 @@ views.ProjectProfile = Backbone.View.extend({
     	// requires Google jsapi script already loaded 
     	google.load("visualization", "1", {packages:["table"], 'callback': function(){
         	var apiUrl = 'https://www.googleapis.com/fusiontables/v2/query'; // Google Fusion Table API endpoint
-          var datasource = '1dXV_8jv2TKA85YlNcpflm7ss1S5uXA0AX6d7Lk-e'; // ID of the Fusion Table we are pulling data from
+          var datasource = '1paho2cEN7uQWIQZecjW6VrBpTXDM1_bK7CP1V-8F'; // ID of the Fusion Table we are pulling data from 
         	var sql = 'SELECT AMOUNT_USD,PO_ID,VENDOR_NAME,VENDOR_CLASSIFICATION,PO_DT,PO_DESCRIPTION,PO_REF FROM ' + datasource + ' WHERE PROJECT = ' + id + ' ORDER BY PO_DT DESC';
         	var key = 'AIzaSyCu3LqZDIDAj5f7uWzIJaI0BESvOxuAuUg'; // Google API key used for requests attribution
         	var mask = {
