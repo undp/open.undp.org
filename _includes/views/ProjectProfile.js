b783ede4bc4689ba91fb5d2862607193c0ada5b9
Jura Khrapunov <jura@khrapunov.com>
2018-03-13 11:02:00 -0400

    Updated POs source file, closes #234

--- _includes/views/ProjectProfile.js
+++ _includes/views/ProjectProfile.js
@@ -229,7 +229,7 @@ views.ProjectProfile = Backbone.View.extend({
     	// requires Google jsapi script already loaded 
     	google.load("visualization", "1", {packages:["table"], 'callback': function(){
         	var apiUrl = 'https://www.googleapis.com/fusiontables/v2/query'; // Google Fusion Table API endpoint
-          var datasource = '1paho2cEN7uQWIQZecjW6VrBpTXDM1_bK7CP1V-8F'; // ID of the Fusion Table we are pulling data from 
+          var datasource = '1vaj0V1u06x_0hxE6yPJ_rA1WaWNqz1szU7wMvQ0E'; // ID of the Fusion Table we are pulling data from 
         	var sql = 'SELECT AMOUNT_USD,PO_ID,VENDOR_NAME,VENDOR_CLASSIFICATION,PO_DT,PO_DESCRIPTION,PO_REF FROM ' + datasource + ' WHERE PROJECT = ' + id + ' ORDER BY PO_DT DESC';
         	var key = 'AIzaSyCu3LqZDIDAj5f7uWzIJaI0BESvOxuAuUg'; // Google API key used for requests attribution
         	var mask = {
